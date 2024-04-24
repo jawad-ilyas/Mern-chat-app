fa6c0d630ef12bd47dc4254177f72049a65e083d
jawad ilyas <mughaljawad12@gmail.com>
2024-04-24 18:38:23 +0500

    basic setup of the shadcn is complete + folder structure + routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Outlet />
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
+const Home = lazy(() => import("./pages/Home.jsx"))
+const Login = lazy(() => import("./pages/Login.jsx"))
+const Groups = lazy(() => import("./pages/Groups.jsx"))
+const Chat = lazy(() => import("./pages/Chat.jsx"))
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "chat/:chatid",
+        element: <Chat />,
+      },
+      {
+        path: "group",
+        element: <Groups />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <HelmetProvider>
+
+
+        <App />
+      </HelmetProvider>
+    </RouterProvider>
   </React.StrictMode>,
 )
