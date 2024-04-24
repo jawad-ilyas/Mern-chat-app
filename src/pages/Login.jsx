de995f466711c0d59042fbb1763fdc87e5610fd6
jawad ilyas <mughaljawad12@gmail.com>
2024-04-24 19:18:19 +0500

    starting a login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div>
+            <div className='flex flex-row'>
+                <div className='w-2/5 bg-white h-screen'></div>
+                <div className='w-3/5 bg-red-400 h-screen'>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
